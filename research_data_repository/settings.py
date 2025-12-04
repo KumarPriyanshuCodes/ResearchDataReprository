4f92df5c2d2eccffa07d804e3453f79c6f7f0f8a
KumarPriyanshuC <priyanshukumar09536@gmail.com>
2025-12-04 22:39:31 +0530

    Fix: added render domain to ALLOWED_HOSTS

--- research_data_repository/settings.py
+++ research_data_repository/settings.py
@@ -25,11 +25,7 @@ SECRET_KEY = 'django-insecure-r81_7f!up&j_yrs+@4oas-4k9-l0g7wb3!c6#2)qj-cz899o=4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    'researchdatarepository-1.onrender.com',
-    'localhost',
-    '127.0.0.1'
-]
+ALLOWED_HOSTS = ["*"]
 
 
 
